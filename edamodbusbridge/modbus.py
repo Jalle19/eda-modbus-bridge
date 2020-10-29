b2c4f51c107a346ba3e35074619306a03d3b10e5
Sam Stenvall <sam.stenvall@nitor.com>
2020-10-29 11:39:17 +0200

    Include registers 11, 56 and 136 in the summary results

--- edamodbusbridge/modbus.py
+++ edamodbusbridge/modbus.py
@@ -70,6 +70,7 @@ class Modbus:
             readings["supplyAirTemperature"] = parse_temperature(result.registers[2])
             readings["wasteAirTemperature"] = parse_temperature(result.registers[3])
             readings["exhaustAirTemperature"] = parse_temperature(result.registers[4])
+            readings["exhaustAirTemperatureBeforeHeatRecovery"] = parse_temperature(result.registers[5])
             readings["exhaustAirHumidity"] = result.registers[7]
             result = await self.modbus_client.read_holding_registers(29, 7, unit=0x01)
             readings["heatRecoverySupplySide"] = result.registers[0]
@@ -81,6 +82,8 @@ class Modbus:
             readings["cascadeSp"] = result.registers[0]
             readings["cascadeP"] = result.registers[1]
             readings["cascadeI"] = result.registers[2]
+            result = await self.modbus_client.read_holding_registers(56, 1, unit=0x01)
+            readings["overPressureTimeLeft"] = result.registers[0]
 
         return readings
 
@@ -119,6 +122,8 @@ class Modbus:
         async with self.modbus_lock:
             result = await self.modbus_client.read_coils(16, 1, unit=0x01)
             info["fanType"] = result.bits[0]
+            result = await self.modbus_client.read_holding_registers(136, 1, unit=0x01)
+            info["heatingConfigurationMode"] = result.registers[0]
             result = await self.modbus_client.read_holding_registers(597, 3, unit=0x01)
             info["familyType"] = result.registers[0]
             info["serialNumber"] = result.registers[1]
